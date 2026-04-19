47777e2dd0e53764dbcead7aca900fd0b4768c24
Manzi Asifiwe <manzi@medway.ai>
2026-04-19 17:04:08 +0200

    Update ALLOWED_HOSTS configuration in settings.py to dynamically set allowed hosts from environment variable, improving deployment flexibility.

--- config/settings.py
+++ config/settings.py
@@ -30,7 +30,7 @@ SECRET_KEY = 'django-insecure-3kg-q-9=locbc#)e=88_m+%wylpp25*ga_w5mp6v2c0d*$%yw+
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = [h.strip() for h in os.getenv("ALLOWED_HOSTS", "localhost,127.0.0.1").split(",") if h.strip()]
 
 
 # Application definition
